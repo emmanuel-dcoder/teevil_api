85fea8076e64e7c1445d07546af4b66795837e36
Noah Emmanuel Idachaba <58103602+codewithemmy@users.noreply.github.com>
2025-02-09 00:44:15 +0100

    new update

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,9 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { config } from './config/env.config';
 import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot(config.database.mongo_url),
-    UserModule,
-    AuthModule,
-  ],
+  imports: [MongooseModule.forRoot(config.database.mongo_url), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
