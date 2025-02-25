34fa7fe0505784094068ba01bf966fe8b90923ba
Noah Emmanuel Idachaba <58103602+codewithemmy@users.noreply.github.com>
2025-02-25 14:04:05 +0100

    new update

--- src/user/schemas/user.schema.ts
+++ src/user/schemas/user.schema.ts
@@ -18,7 +18,7 @@ export class User {
   @Prop({ required: true })
   password: string;
 
-  @Prop({ required: true, unique: true })
+  @Prop({ required: true })
   email: string;
 
   @Prop({ required: true })
